fcd5503e1c8f168f0883348d0f979b20b370efcd
Devansh Tikariha <117193000+devanshtikariha03@users.noreply.github.com>
2023-06-23 20:47:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -106,5 +106,12 @@ def update_graph(metric):
     
     return figure
 
-if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+app = Dash(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello!</h1>"
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
