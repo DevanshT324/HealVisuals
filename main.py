a2ee35eeeea9a6e950d64dc19201af5f9905e915
Raghav Sivakuru <137376409+raghavcannotfly@users.noreply.github.com>
2023-06-24 21:47:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ numerical_cols = data.select_dtypes(include='number').columns
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-@app.route('/')
 app.title = "HealVisuals: Discovering Health Through Data"
 
 app.layout = html.Div([
